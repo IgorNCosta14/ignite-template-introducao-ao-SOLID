a5fb5e3737f71ef94c2d52538f98c26174d06531
Igor Nascimento costa <igorncosta14@hotmail.com>
2022-03-07 20:50:04 -0300

    07.03

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import { Request, Response } from "express";
 
 import { ListAllUsersUseCase } from "./ListAllUsersUseCase";
@@ -6,7 +7,18 @@ class ListAllUsersController {
   constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    const { user_id } = request.headers;
+    const id = user_id.toString();
+
+    
+    try {
+      const users = this.listAllUsersUseCase.execute({user_id: id})
+
+      return response.status(201).json(users)
+
+    } catch (error) {
+      return response.status(400).json({ error: error.message })
+    }
   }
 }
 
